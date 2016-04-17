5c65bb06f24070d6c4df95b6638c489065efd394
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-04-17 16:28:21 +0100

    Bug fix

--- js/syntactic.js
+++ js/syntactic.js
@@ -1,286 +1,138 @@
-var SyntacticAnalysis = function(sequence) {
-	this.declarationIndex = 0;
-	this.newIndex = 0;
-	this.expIndex = 0;
-	this.termIndex = 0;
-	this.term_Index = 0;
-	this.factorIndex = 0;
-	this.factor_Index = 0;
+var Syntactic = function(sequence) {
 	this.sequence = sequence;
 	this.tree = null;
+	this.opIndex = 0;
+
 	this.syntacticAnalysis();
 	console.log(this.tree);
-}
+};
 
-SyntacticAnalysis.prototype.syntacticAnalysis = function() {
+Syntactic.prototype.syntacticAnalysis = function() {
 	if(this.sequence.tokens.length == 0)
 		return;
 
 	if(this.S())
 		console.log("Correct Syntactic construction of the statement.");
-	else 
+	else
 		console.error("Error on statement's construction.");
 };
 
-SyntacticAnalysis.prototype.S = function() {
+Syntactic.prototype.S = function() {
 	this.tree = new Tree('START');
 	var loop = true;
-	do {
-		if(!this.A())
+	var i = 0;
+
+	while(this.sequence.peek() != null){
+
+		if(!this.expressionHandler(i++)) {
+			this.tree = null;
 			return false;
-		this.sequence.nextToken();
-		if(TOKENS[this.sequence.peek().id] == TOKENS.SEMI_COLON) 
-			this.sequence.nextToken();
-		else loop = false;
-	} while(loop);
-	
-	return this.DUMP();
-};
-
-SyntacticAnalysis.prototype.A = function() {
-	var nodeId = 'DECLARATION' + this.declarationIndex++;
-	this.tree.add(nodeId, 'START', this.tree.traverseDF);
-
-	if(TOKENS[this.sequence.peek().id] == TOKENS.DECLARATION) {
-		this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-		this.sequence.nextToken();
-		if(TOKENS[this.sequence.peek().id] == TOKENS.IDENTIFIER) {
-			this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-			this.sequence.nextToken();
-			if(TOKENS[this.sequence.peek().id] == TOKENS.EQUAL) {
-				this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-				this.sequence.nextToken();
-				var sequenceIndex = this.sequence.i;
-				if(this.New(nodeId))
-					return true;
-				else if(this.Exp(nodeId, sequenceIndex))
-					return true;
-			}
 		}
+
+		this.sequence.nextToken();
 	}
 
-	this.tree.remove(nodeId, 'START', this.tree.traverseDF);
+	return true;
+};
+
+Syntactic.prototype.expressionHandler = function(parent) {
+	this.tree.add(parent, 'START', this.tree.traverseDF);
+
+	if(this.sequence.peek().id == TOKENS.DECLARATION) {
+		this.sequence.nextToken();
+		return this.declaration(parent);
+	}
+	return this.dump(parent);
+};
+
+Syntactic.prototype.declaration = function(parent) {
+	var identifier = null;
+	var equalsId = null;
+	console.log("DECLARATION");
+	if(this.sequence.peek().id == TOKENS.IDENTIFIER) {
+		identifier = this.sequence.peek();
+		this.sequence.nextToken();
+		if(this.sequence.peek().id == TOKENS.EQUAL) {
+			equalsId = this.sequence.peek().img + parent;
+			this.tree.add(equalsId, parent, this.tree.traverseDF);
+			if (!this.declarationHandler(equalsId)) {
+				return false;
+			}
+			this.tree.addBegining(identifier, equalsId, this.tree.traverseDF);
+			return true;
+		}
+	}
 	return false;
 };
 
-SyntacticAnalysis.prototype.New = function(parent) {
-	var nodeId = 'NEW' + this.newIndex++;
-	this.tree.add(nodeId, parent, this.tree.traverseDF);
+Syntactic.prototype.declarationHandler = function(parent) {
+	this.sequence.nextToken();
+	if(this.sequence.peek().id == TOKENS.NEW) {
+		return this.newExpression(parent);
+	}
+	else{
+		var data = this.operation(parent, this.tree);
+		console.log("Data :" );
+		console.log(data);
+		if (data != null) {
+			if (this.sequence.peek().id == TOKENS.SEMI_COLON) {
+			  return data;
+		  }
+		}
 
-	if(TOKENS[this.sequence.peek().id] == TOKENS.NEW) {
-		this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+		//should never happen
+		return false;
+	}
+
+	return false;
+};
+
+Syntactic.prototype.newExpression = function(parent) {
+	this.sequence.nextToken();
+	if(this.sequence.peek().id == TOKENS.OPEN) {
 		this.sequence.nextToken();
-		if(TOKENS[this.sequence.peek().id] == TOKENS.OPEN) {
-			this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+		if(this.sequence.peek().id == TOKENS.QUOTE) {
 			this.sequence.nextToken();
-			if(TOKENS[this.sequence.peek().id] == TOKENS.QUOTE) {
-				this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+			if(this.sequence.peek().id == TOKENS.FILENAME) {
+				this.tree.add(this.sequence.peek(), parent, this.tree.traverseDF);
 				this.sequence.nextToken();
-				if(TOKENS[this.sequence.peek().id] == TOKENS.FILENAME) {
-					this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+				if(this.sequence.peek().id == TOKENS.QUOTE) {
 					this.sequence.nextToken();
-					if(TOKENS[this.sequence.peek().id] == TOKENS.QUOTE) {
-						this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+					if(this.sequence.peek().id == TOKENS.CLOSE) {
 						this.sequence.nextToken();
-						if(TOKENS[this.sequence.peek().id] == TOKENS.CLOSE) {
-							this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-							return true;
-						}
+						return this.sequence.peek().id == TOKENS.SEMI_COLON;
 					}
 				}
 			}
 		}
 	}
-
-	this.tree.remove(nodeId, parent, this.tree.traverseDF);
 	return false;
 };
 
-SyntacticAnalysis.prototype.Exp = function(parent, sequenceIndex) {
-	this.sequence.i = sequenceIndex;
-	var nodeId = 'EXPRESSION' + this.expIndex++;
-	this.tree.add(nodeId, parent, this.tree.traverseDF);
-
-	if(this.Term(nodeId)) {
+Syntactic.prototype.dump = function(parent) {
+	console.log(this.sequence.peek().img);
+	if(this.sequence.peek().id == TOKENS.IDENTIFIER) {
+		var filename = this.sequence.peek();
 		this.sequence.nextToken();
-		return this.Term_(nodeId);
-	}
-
-	this.tree.remove(nodeId, parent, this.tree.traverseDF);
-	return false;
-};
-
-SyntacticAnalysis.prototype.Term_ = function(parent) {
-	var nodeId = 'TERM_' + this.term_Index++;
-	this.tree.add(nodeId, parent, this.tree.traverseDF);
-
-	if(this.OP1(nodeId)) {
-		this.sequence.nextToken();
-		if(this.Term(nodeId)) {
+		if(this.sequence.peek().id == TOKENS.CONCATENATE) {
 			this.sequence.nextToken();
-			if(this.Term_(nodeId)) {
-				return true;
-			}
-		}
-	}
-	else {
-		this.tree.remove(nodeId, parent, this.tree.traverseDF);
-		this.sequence.i--;
-		return true;
-	}
-};
-
-SyntacticAnalysis.prototype.Term = function(parent) {
-	var nodeId = 'TERM' + this.termIndex++;
-	this.tree.add(nodeId, parent, this.tree.traverseDF);
-
-	if(this.Factor(nodeId)) {
-		this.sequence.nextToken();
-		if(this.Factor_(nodeId)) {
-			return true;
-		}
-	}
-
-	this.tree.remove(nodeId, parent, this.tree.traverseDF);
-	return false;
-};
-
-SyntacticAnalysis.prototype.Factor_ = function(parent) {
-	var nodeId = 'FACTOR_' + this.factor_Index++;
-	this.tree.add(nodeId, parent, this.tree.traverseDF);
-
-	if(this.OP2(nodeId)) {
-		this.sequence.nextToken();
-		if(this.Factor(nodeId)) {
-			this.sequence.nextToken();
-			if(this.Factor_(nodeId)) {
-				return true;
-			}
-		}
-	}
-	else {
-		this.tree.remove(nodeId, parent, this.tree.traverseDF);
-		this.sequence.i--;
-		return true;
-	}
-};
-
-SyntacticAnalysis.prototype.Factor = function(parent) {
-	var nodeId = 'FACTOR' + this.factorIndex++;
-	this.tree.add(nodeId, parent, this.tree.traverseDF);
-
-	if(TOKENS[this.sequence.peek().id] == TOKENS.IDENTIFIER){
-		this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-		return true;
-	}
-	else if(TOKENS[this.sequence.peek().id] == TOKENS.OPEN) {
-		this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-		this.sequence.nextToken();
-		if(this.Exp(nodeId, this.sequence.i)) {
-			this.sequence.nextToken();
-			if(TOKENS[this.sequence.peek().id] == TOKENS.CLOSE) {
-				this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-				return true;
-			}
-		}
-	}
-	else if(TOKENS[this.sequence.peek().id] == TOKENS.COMPLEMENT) {
-		this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-		this.sequence.nextToken();
-		if(TOKENS[this.sequence.peek().id] == TOKENS.OPEN) {
-			this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-			this.sequence.nextToken();
-			if(this.Exp(nodeId, this.sequence.i)) {
+			if(this.sequence.peek().id == TOKENS.DUMP) {
+				var dumpId = this.sequence.peek().img + parent;
+				this.tree.add(dumpId, parent, this.tree.traverseDF);
+				this.tree.add(filename, dumpId, this.tree.traverseDF);
 				this.sequence.nextToken();
-				if(TOKENS[this.sequence.peek().id] == TOKENS.CLOSE) {
-					this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-					return true;
-				}
-			}
-		}
-	}
-	else if(TOKENS[this.sequence.peek().id] == TOKENS.REVERSE) {
-		this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-		this.sequence.nextToken();
-		if(TOKENS[this.sequence.peek().id] == TOKENS.OPEN) {
-			this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-			this.sequence.nextToken();
-			if(this.Exp(nodeId, this.sequence.i)) {
-				this.sequence.nextToken();
-				if(TOKENS[this.sequence.peek().id] == TOKENS.CLOSE) {
-					this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-					return true;
-				}
-			}
-		}
-	}
-
-
-	this.tree.remove(nodeId, parent, this.tree.traverseDF);
-	return false;
-};
-
-SyntacticAnalysis.prototype.OP1 = function(parent) {
-	var nodeId = 'OP1';
-	this.tree.add(nodeId, parent, this.tree.traverseDF);
-
-	if(TOKENS[this.sequence.peek().id] == TOKENS.CONCATENATE 
-		|| TOKENS[this.sequence.peek().id] == TOKENS.UNION) {
-
-		this.tree.add(this.sequence,peek().img, nodeId, this.tree.traverseDF);
-		return true;
-	}
-
-	this.tree.remove(nodeId, parent, this.tree.traverseDF);
-	return false;
-}
-
-SyntacticAnalysis.prototype.OP2 = function(parent) {
-	var nodeId = 'OP1';
-	this.tree.add(nodeId, parent, this.tree.traverseDF);
-
-	if(TOKENS[this.sequence.peek().id] == TOKENS.INTERSECTION 
-		|| TOKENS[this.sequence.peek().id] == TOKENS.MULTIPLY) {
-
-		this.tree.add(this.sequence,peek().img, nodeId, this.tree.traverseDF);
-		return true;
-	}
-	
-	this.tree.remove(nodeId, parent, this.tree.traverseDF);
-	return false;
-}
-
-SyntacticAnalysis.prototype.DUMP = function() {
-	var nodeId = 'DUMP';
-	this.tree.add(nodeId, 'START', this.tree.traverseDF);
-	if(TOKENS[this.sequence.peek().id] == TOKENS.IDENTIFIER) {
-		this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-		this.sequence.nextToken();
-		if(TOKENS[this.sequence.peek().id] == TOKENS.CONCATENATE) {
-			this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-			this.sequence.nextToken();
-			if(TOKENS[this.sequence.peek().id] == TOKENS.DUMP) {
-				this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-				this.sequence.nextToken();
-				if(TOKENS[this.sequence.peek().id] == TOKENS.OPEN) {
-					this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+				if(this.sequence.peek().id == TOKENS.OPEN) {
 					this.sequence.nextToken();
-					if(TOKENS[this.sequence.peek().id] == TOKENS.QUOTE) {
-						this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+					if(this.sequence.peek().id == TOKENS.QUOTE) {
 						this.sequence.nextToken();
-						if(TOKENS[this.sequence.peek().id] == TOKENS.IDENTIFIER) {
-							this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+						if(this.sequence.peek().id == TOKENS.IDENTIFIER) {
+							this.tree.add(this.sequence.peek(), dumpId, this.tree.traverseDF);
 							this.sequence.nextToken();
-							if(TOKENS[this.sequence.peek().id] == TOKENS.QUOTE) {
-								this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+							if(this.sequence.peek().id == TOKENS.QUOTE) {
 								this.sequence.nextToken();
-								if(TOKENS[this.sequence.peek().id] == TOKENS.CLOSE) {
-									this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
+								if(this.sequence.peek().id == TOKENS.CLOSE) {
 									this.sequence.nextToken();
-									if(TOKENS[this.sequence.peek().id] == TOKENS.SEMI_COLON) {
-										this.tree.add(this.sequence.peek().img, nodeId, this.tree.traverseDF);
-										return true;
-									}
+									return this.sequence.peek().id == TOKENS.SEMI_COLON;
 								}
 							}
 						}
@@ -289,6 +141,154 @@ SyntacticAnalysis.prototype.DUMP = function() {
 			}
 		}
 	}
-	
+
 	return false;
-}
+};
+
+
+//retorna arvore modificada ou null em caso de erro
+Syntactic.prototype.operation = function(parent, tree_) {
+	console.log("OPERATION");
+	if (this.sequence.peek().id == TOKENS.COMPLEMENT) {
+
+		newTree = this.checkComplement(parent);
+		if(newTree != null){
+			var newParentTree = this.operationR(parent, tree_);
+			if(newParentTree != null)
+			{
+				newParentTree.addTreeBegining(newTree, newParentTree._root.data, tree_.traverseDF);
+				tree_.addTree(newParentTree, parent, tree_.traverseDF);
+
+				tree_.addTree(newTree, parent, tree_.traverseDF);
+				return tree_;
+			}
+		}
+
+	} else if (this.sequence.peek().id == TOKENS.REVERSE) {
+
+		newTree = this.checkReverse(parent);
+		if(newTree != null){
+			var newParentTree = this.operationR(parent, tree_);
+			//TODO operationR só retorna null em caso de erro
+			//corrigir isto
+			if(newParentNode != null)
+			{
+				newParentTree.addTreeBegining(newTree, newParentTree._root.data, tree_.traverseDF);
+				return newParentTree;
+			}
+
+			tree_.addTree(newTree, parent, tree_.traverseDF);
+			return tree_;
+		}
+
+	} else if (this.sequence.peek().id == TOKENS.IDENTIFIER) {
+		console.log("IDENTIFIER");
+		var ident = this.sequence.peek().img;
+		this.sequence.nextToken();
+		var newParentNode = this.operationR(parent, tree_);
+		if (newParentNode != null){
+			tree_.addTree(newParentNode, parent, tree_.traverseDF);
+			tree_.addBegining(ident, newParentNode._root.data, tree_.traverseDF);
+			tree_.add(ident, parent, tree_.traverseDF);
+			return tree_;
+		}
+
+	} else if (this.sequence.peek().id == TOKENS.OPEN) {
+		this.sequence.nextToken();
+		newTree = this.checkParenteses(parent);
+		if(newTree != null){
+			var newParentTree = this.operationR(parent, tree_);
+			if(newParentNode != null)
+			{
+				newParentTree.addTree(newTree, newParentTree._root, tree_.traverseDF);
+
+				tree_.addTree(newTree, parent, tree_.traverseDF);
+				return tree_;
+			}
+		}
+	}
+
+	return null;
+};
+
+Syntactic.prototype.checkReverse = function(parent) {
+	if(this.sequence.peek().id == TOKENS.REVERSE) {
+		var complementId = this.sequence.peek().img + this.opIndex++;
+		var newTree = new Tree(complementId);
+		this.sequence.nextToken();
+		if(this.sequence.peek().id == TOKENS.OPEN) {
+			this.sequence.nextToken();
+			var newTree_ = this.operation(complementId, newTree); //returns a updated version of 'newTree' with all the nodes of the expression inside the not operation
+			if (newTree_ != null) {
+				if (this.sequence.peek().id == TOKENS.CLOSE) {
+					this.sequence.nextToken();
+					return newTree_;
+				}
+			}
+		}
+	}
+
+	return null;
+};
+
+Syntactic.prototype.checkComplement = function(parent) {
+	if(this.sequence.peek().id == TOKENS.COMPLEMENT) {
+		var complementId = this.sequence.peek().img + this.opIndex++;
+		var newTree = new Tree(complementId);
+		this.sequence.nextToken();
+		if(this.sequence.peek().id == TOKENS.OPEN) {
+			this.sequence.nextToken();
+			var newTree_ = this.operation(complementId, newTree); //returns a updated version of 'newTree' with all the nodes of the expression inside the not operation
+			if (newTree_ != null) {
+				if (this.sequence.peek().id == TOKENS.CLOSE) {
+					this.sequence.nextToken();
+					return newTree_;
+				}
+			}
+		}
+	}
+
+	return null;
+};
+
+
+Syntactic.prototype.checkParenteses = function(parent) {
+		var newTree_ = this.operation(parent, null); //returns a updated version of 'newTree' with all the nodes of the expression inside the not operation
+		if (newTree_ != null) {
+			if (this.sequence.peek().id == TOKENS.CLOSE) {
+				this.sequence.nextToken();
+				return newTree_;
+			}
+		}
+
+	return null;
+};
+
+
+// retorna o novo parentNode respetivo à OPC detetada. se nao houver qualquer OPC, retorna null
+Syntactic.prototype.operationR = function(parent) {
+	var tokenOP = this.checkOPC();
+	var tokenOPId = "operationR" + this.opIndex++;
+	var newTree = new Tree(tokenOPId);
+
+	if (tokenOP != null) {
+		tokenOPId = tokenOP.img + this.opIndex++;
+		newTree = new Tree(tokenOPId);
+		this.sequence.nextToken();
+		data = this.operation(tokenOPId, newTree);
+		return data;
+	}
+
+	return newTree;
+};
+
+Syntactic.prototype.checkOPC = function() {
+	if (this.sequence.peek().id == TOKENS.MULTIPLY
+		|| this.sequence.peek().id == TOKENS.CONCATENATE
+		|| this.sequence.peek().id == TOKENS.INTERSECTION
+		|| this.sequence.peek().id == TOKENS.UNION) {
+			return this.sequence.peek();
+		}
+
+		return null;
+};
