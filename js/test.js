1584134640ea8804bf91aceed32022b459e18776
Rui Vilares <rui.vilares@hotmail.com>
2016-05-10 23:35:39 +0100

    several site changes!

--- js/test.js
+++ js/test.js
@@ -3,9 +3,18 @@
 function read(){
 // provide data in the DOT language
 var DOTstring = 'dinetwork {'+
-  'A1 -> B1 [label=f];' + 
-  'A2 -> B2 [label=f]; ' + 
-  'A1 -> A2 [label=f]}';
+  'A1 -> B1 [label=1];' +
+  'A1 -> A1 [label=0]; ' +
+  'B1 -> A1 [label=0];'+
+ 'A1[ color=red, shape=triangle]'+
+'}';
+
+var DOTstringB = 'dinetwork {'+
+  'A1 -> B1 [label=0];' +
+  'A1 -> A1 [label=1]; ' +
+  'B1 -> A1 [label=1];'+
+ 'A1[ color=red, shape=triangle]'+
+'}';
 
 var parsedData = vis.network.convertDot(DOTstring);
 var container = document.getElementById('mynetwork');
@@ -23,4 +32,27 @@ options.nodes = {
 
 // create a network
 var network = new vis.Network(container, data, options);
+};
+
+//ve se tem estado inicial e final
+function checkNodes(){
+	var initialState=searchStringInArray('shape: triangle',nodes);
+	if(searchStringInArray!=1){
+		// TODO
+		window.alert('Sem estado inicial');
+	}
+	var finalState=searchStringInArray('shape: circle',nodes);
+	var finalState1=searchStringInArray('shapeProperties:{borderDashes:true}',nodes);
+	if(finalState!=1 &&	finalState1!=1){
+		window.alert('Sem estado final');
+	}
+
+};
+function searchStringInArray (str, strArray) {
+	var count=0;
+    for (var j=0; j<strArray.length; j++) {
+        if (strArray[j].match(str)) return j;
+        count++;
+    }
+    return count;
 };
