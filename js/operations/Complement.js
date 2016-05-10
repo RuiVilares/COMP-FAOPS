d1ffc00e0aee256d7858ef20dff7a67fcd05b957
Rui Vilares <rui.vilares@hotmail.com>
2016-05-11 00:23:13 +0100

    Complement operation done!

--- js/operations/Complement.js
+++ js/operations/Complement.js
@@ -1,3 +1,48 @@
-function complement(node) {
-  
+//Constructor (only one dfa are parsed)
+var Complement = function complement(DFA) {
+  var DOTstring = 'dinetwork {' +
+      'A -> B [label="a"];' +
+      'A -> A [label="b"];' +
+      'B -> B [label="b"];' +
+      'B -> A [label="a"];' +
+      'A[ color=red, shape=triangle]' +
+      'B[ color=blue, shape=circle]' +
+      '}';
+  this.DFA = this.parseDFA(DOTstring);
+};
+
+Complement.prototype.compute = function() {
+  var nodes = this.DFA.data.nodes;
+  for (var i = 0; i < nodes.length; i++) {
+    if (nodes[i].color != null && nodes[i].color.background == "red") {
+      this.DFA.data.nodes[i].color.background = "blue";
+    }
+    if (nodes[i].color != null && nodes[i].color.background == "blue") {
+      this.DFA.data.nodes[i].color.background = "red";
+    }
+  }
+
+  return this.DFA;
+
 }
+
+//parse a string to dfa
+Complement.prototype.parseDFA = function(str) {
+	var parsedData = vis.network.convertDot(str);
+
+  var data = {
+    nodes: parsedData.nodes,
+    edges: parsedData.edges
+  }
+
+  var options = parsedData.options;
+
+  options.nodes = {
+    color: 'red'
+  }
+
+  var dfa_test = new DFA(options);
+  dfa_test.setData(data);
+
+  return dfa_test;
+};
