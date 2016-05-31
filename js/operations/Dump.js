40e506a1fb5d8a959c2adc4e0417ad7764e02948
guilhermevpinto <guilhermevpinto@gmail.com>
2016-05-31 22:03:06 +0100

    dump fix

--- js/operations/Dump.js
+++ js/operations/Dump.js
@@ -7,35 +7,16 @@ Dump.prototype.compute = function(){
   var nodes = this.dfa.data.nodes;
   var edges = this.dfa.data.edges;
 
-  for(var i=0; i < nodes.length; i++){
-    content+= '  '+nodes[i].id +"[color="+ nodes[i].color.background +", shape="+nodes[i].shape+ "]\n";
+  for(var i=0; i< edges.length; i++){
+    content+='  "'+edges[i].from + "\"->\""+ edges[i].to + "\"[label=\""+ edges[i].label+"\"];\n"
   }
 
-  for(var i=0; i< edges.length; i++){
-    content+='  '+edges[i].from + "->"+ edges[i].to + "[label="+ edges[i].label+"];\n"
+  for(var i=0; i < nodes.length; i++){
+    content+= '  "'+nodes[i].id +"\"[color="+ nodes[i].color.background +", shape="+nodes[i].shape+ "]\n";
   }
+
   content+='}';
+  console.log(content);
   return content;
 
-}
-
-//parse a string to dfa
-Dump.prototype.parseDFA = function (str) {
-    var parsedData = vis.network.convertDot(str);
-
-    var data = {
-        nodes: parsedData.nodes,
-        edges: parsedData.edges
-    }
-
-    var options = parsedData.options;
-
-    options.nodes = {
-        color: 'red'
-    }
-
-    var dfa_test = new DFA(options);
-    dfa_test.setData(data);
-
-    return dfa_test;
 };
